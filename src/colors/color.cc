eeace0063951016c074ac75f8791e92080019cf3
Erwan Auer <erwan.auer@epita.fr>
2018-09-16 15:37:51 +0200

    [COLOR] added color class and utilities
    Added Color struct, multiplication with scalar and color, addition and
    substraction

---
+++ src/colors/color.cc
@@ -0,0 +1,76 @@
+#include "color.hh"
+
+namespace
+{
+    bool equal(float a, float b)
+    {
+        return abs(a - b) < EPSILON;
+    }
+}
+
+Color::Color(float red, float green, float blue)
+    : red_(red), green_(green), blue_(blue)
+{}
+
+Color& Color::operator+=(const Color& rhs)
+{
+    red_ += rhs.red_;
+    green_ += rhs.green_;
+    blue_ += rhs.blue_;
+    return *this;
+}
+
+Color& Color::operator-=(const Color& rhs)
+{
+    red_ -= rhs.red_;
+    green_ -= rhs.green_;
+    blue_ -= rhs.blue_;
+    return *this;
+}
+
+Color& Color::operator*=(float rhs)
+{
+    red_ *= rhs;
+    green_ *= rhs;
+    blue_ *= rhs;
+    return *this;
+}
+
+Color& Color::operator*=(const Color& rhs)
+{
+    red_ *= rhs.red_;
+    green_ *= rhs.green_;
+    blue_ *= rhs.blue_;
+    return *this;
+}
+
+bool operator==(const Color& lhs, const Color& rhs)
+{
+    return equal(lhs.red_, rhs.red_) && equal(lhs.green_, rhs.green_)
+        && equal(lhs.blue_, rhs.blue_);
+}
+
+Color operator+(Color lhs, const Color& rhs)
+{
+    return lhs += rhs;
+}
+
+Color operator-(Color lhs, const Color& rhs)
+{
+    return lhs -= rhs;
+}
+
+Color operator*(Color lhs, float rhs)
+{
+    return lhs *= rhs;
+}
+
+Color operator*(Color lhs, const Color& rhs)
+{
+    return lhs *= rhs;
+}
+
+Color color(float red, float green, float blue)
+{
+    return Color(red, green, blue);
+}
---
+++ src/colors/color.hh
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cmath>
+
+#include "constants.hh"
+
+struct Color
+{
+    Color(float red, float green, float blue);
+
+    Color& operator+=(const Color& rhs);
+    Color& operator-=(const Color& rhs);
+    Color& operator*=(float rhs);
+    Color& operator*=(const Color& rhs);
+
+    float red_;
+    float green_;
+    float blue_;
+};
+
+bool operator==(const Color& lhs, const Color& rhs);
+
+Color operator+(Color lhs, const Color& rhs);
+Color operator-(Color lhs, const Color& rhs);
+Color operator*(Color lhs, float rhs);
+Color operator*(Color lhs, const Color& rhs);
+
+Color color(float red, float green, float blue);
