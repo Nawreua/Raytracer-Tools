c615b8f43bd43faf1d8ce5f2f56dc2848bc4999f
Erwan Auer <erwan.auer@epita.fr>
2019-02-27 02:21:16 +0100

    [OFFSIDE] fixed errors due to changes in Section 9

--- offside/3DSphere/main.cc
+++ offside/3DSphere/main.cc
@@ -7,6 +7,7 @@
 #include "material.hh"
 #include "matrix.hh"
 #include "ray.hh"
+#include "shape.hh"
 #include "sphere.hh"
 #include "tuple.hh"
 
@@ -15,53 +16,53 @@ int main()
     auto world = World();
     world.lights_.push_back(PointLight(point(-10, 10, -10), Color(1, 1, 1)));
 
-    auto floor = Sphere();
-    floor.set_transform(scaling(10, 0.01, 10));
-    floor.material_ = Material();
-    floor.material_.color_ = Color(1, 0.9, 0.9);
-    floor.material_.specular_ = 0;
+    auto floor = std::make_shared<Sphere>();
+    floor->set_transform(scaling(10, 0.01, 10));
+    floor->material_ = Material();
+    floor->material_.color_ = Color(1, 0.9, 0.9);
+    floor->material_.specular_ = 0;
 
     world.objects_.push_back(floor);
 
-    auto left_wall = Sphere();
-    left_wall.set_transform(translation(0, 0, 5) * rotation_y(- PI / 4)                
+    auto left_wall = std::make_shared<Sphere>();
+    left_wall->set_transform(translation(0, 0, 5) * rotation_y(- PI / 4)                
             * rotation_x(PI / 2) * scaling(10, 0.1, 10));
-    left_wall.material_ = floor.material_;
+    left_wall->material_ = floor->material_;
 
     world.objects_.push_back(left_wall);
 
-    auto right_wall = Sphere();
-    right_wall.set_transform(translation(0, 0, 5) * rotation_y(PI / 4)
+    auto right_wall = std::make_shared<Sphere>();
+    right_wall->set_transform(translation(0, 0, 5) * rotation_y(PI / 4)
                             * rotation_x(PI / 2) * scaling(10, 0.1, 10));
-    right_wall.material_ = floor.material_;
+    right_wall->material_ = floor->material_;
 
     world.objects_.push_back(right_wall);
 
-    auto middle = Sphere();
-    middle.set_transform(translation(-0.5, 1, 0.5));
-    middle.material_ = Material();
-    middle.material_.color_ = Color(0.1, 1, 0.5);
-    middle.material_.diffuse_ = 0.7;
-    middle.material_.specular_ = 0.3;
+    auto middle = std::make_shared<Sphere>();
+    middle->set_transform(translation(-0.5, 1, 0.5));
+    middle->material_ = Material();
+    middle->material_.color_ = Color(0.1, 1, 0.5);
+    middle->material_.diffuse_ = 0.7;
+    middle->material_.specular_ = 0.3;
 
     world.objects_.push_back(middle);
 
-    auto right = Sphere();
-    right.set_transform(translation(1.5, 0.5, -0.5) * scaling(0.5, 0.5, 0.5));
-    right.material_ = Material();
-    right.material_.color_ = Color(0.5, 1, 0.1);
-    right.material_.diffuse_ = 0.7;
-    right.material_.specular_ = 0.3;
+    auto right = std::make_shared<Sphere>();
+    right->set_transform(translation(1.5, 0.5, -0.5) * scaling(0.5, 0.5, 0.5));
+    right->material_ = Material();
+    right->material_.color_ = Color(0.5, 1, 0.1);
+    right->material_.diffuse_ = 0.7;
+    right->material_.specular_ = 0.3;
 
     world.objects_.push_back(right);
 
-    auto left = Sphere();
-    left.set_transform(translation(-1.5, 0.33, -0.75)
+    auto left = std::make_shared<Sphere>();
+    left->set_transform(translation(-1.5, 0.33, -0.75)
             * scaling(0.33, 0.33, 0.33));
-    left.material_ = Material();
-    left.material_.color_ = Color(1, 0.8, 0.1);
-    left.material_.diffuse_ = 0.7;
-    left.material_.specular_ = 0.3;
+    left->material_ = Material();
+    left->material_.color_ = Color(1, 0.8, 0.1);
+    left->material_.diffuse_ = 0.7;
+    left->material_.specular_ = 0.3;
 
     world.objects_.push_back(left);
 
