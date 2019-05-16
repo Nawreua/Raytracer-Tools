b6cfccc983ec8f5f1231b7ae1c3eaf2930ee42d6
Erwan Auer <erwan.auer@epita.fr>
2019-05-16 15:17:45 +0200

    patterns: add 2 patterns, SolidPattern and BlendedPattern

--- src/pattern/pattern.cc
+++ src/pattern/pattern.cc
@@ -6,7 +6,8 @@ Pattern::Pattern()
     : transform_(Matrix::identity_matrix())
 {}
 
-Color Pattern::pattern_at_shape(const Shape& object, const Tuple& world_point)
+Color Pattern::pattern_at_shape(const Shape& object,
+                                const Tuple& world_point) const
 {
     auto object_point = object.transform_.inverse() * world_point;
     auto pattern_point = transform_.inverse() * object_point;
@@ -18,7 +19,7 @@ void Pattern::set_pattern_transform(const Matrix& t)
     transform_ = t;
 }
 
-Color TestPattern::pattern_at(const Tuple& point)
+Color TestPattern::pattern_at(const Tuple& point) const
 {
     return Color(point.x_, point.y_, point.z_);
 }
@@ -28,7 +29,7 @@ StripePattern::StripePattern(const Color& a, const Color& b)
     , b_(b)
 {}
 
-Color StripePattern::pattern_at(const Tuple& point)
+Color StripePattern::pattern_at(const Tuple& point) const
 {
     return static_cast<int>(floor(point.x_)) % 2 == 0 ? a_ : b_;
 }
@@ -38,7 +39,7 @@ GradientPattern::GradientPattern(const Color& a, const Color& b)
     , b_(b)
 {}
 
-Color GradientPattern::pattern_at(const Tuple& point)
+Color GradientPattern::pattern_at(const Tuple& point) const
 {
     auto distance = b_ - a_;
     auto fraction = point.x_ - floor(point.x_);
@@ -50,7 +51,7 @@ RingPattern::RingPattern(const Color& a, const Color& b)
     , b_(b)
 {}
 
-Color RingPattern::pattern_at(const Tuple& point)
+Color RingPattern::pattern_at(const Tuple& point) const
 {
     int val = floor(sqrt(point.x_ * point.x_ + point.z_ * point.z_));
     return val % 2 == 0 ? a_ : b_;
@@ -61,7 +62,7 @@ CheckersPattern::CheckersPattern(const Color& a, const Color& b)
     , b_(b)
 {}
 
-Color CheckersPattern::pattern_at(const Tuple& point)
+Color CheckersPattern::pattern_at(const Tuple& point) const
 {
     int val = floor(point.x_) + floor(point.y_) + floor(point.z_);
     return val % 2 == 0 ? a_ : b_;
@@ -72,10 +73,31 @@ RadialGradientPattern::RadialGradientPattern(const Color& a, const Color& b)
     , b_(b)
 {}
 
-Color RadialGradientPattern::pattern_at(const Tuple& point)
+Color RadialGradientPattern::pattern_at(const Tuple& point) const
 {
     auto distance = b_ - a_;
     // auto fraction = point.x_ - floor(point.x_);
     int val = floor(sqrt(point.x_ * point.x_ + point.z_ * point.z_));
     return a_ + distance * val;
+}
+
+SolidPattern::SolidPattern(const Color& color)
+    : color_(color)
+{}
+
+Color SolidPattern::pattern_at(const Tuple&) const
+{
+    return color_;
+}
+
+BlendedPattern::BlendedPattern(Pattern& a, Pattern& b)
+    : a_(&a)
+    , b_(&b)
+{}
+
+Color BlendedPattern::pattern_at(const Tuple& point) const
+{
+    auto color_a = a_->pattern_at(point);
+    auto color_b = b_->pattern_at(point);
+    return (color_a + color_b) / 2;
 }
--- src/pattern/pattern.hh
+++ src/pattern/pattern.hh
@@ -11,8 +11,8 @@ class Pattern
 public:
     Pattern();
 
-    virtual Color pattern_at(const Tuple& point) = 0;
-    Color pattern_at_shape(const Shape& object, const Tuple& world_point);
+    virtual Color pattern_at(const Tuple& point) const = 0;
+    Color pattern_at_shape(const Shape& object, const Tuple& world_point) const;
     void set_pattern_transform(const Matrix& t);
 
     Matrix transform_;
@@ -21,7 +21,7 @@ public:
 class TestPattern : public Pattern
 {
 public:
-    Color pattern_at(const Tuple& point) override;
+    Color pattern_at(const Tuple& point) const override;
 };
 
 class StripePattern : public Pattern
@@ -29,7 +29,7 @@ class StripePattern : public Pattern
 public:
     StripePattern(const Color& a, const Color& b);
 
-    Color pattern_at(const Tuple& point) override;
+    Color pattern_at(const Tuple& point) const override;
 
     Color a_;
     Color b_;
@@ -40,7 +40,7 @@ class GradientPattern : public Pattern
 public:
     GradientPattern(const Color& a, const Color& b);
 
-    Color pattern_at(const Tuple& point) override;
+    Color pattern_at(const Tuple& point) const override;
 
     Color a_;
     Color b_;
@@ -51,7 +51,7 @@ class RingPattern : public Pattern
 public:
     RingPattern(const Color& a, const Color& b);
 
-    Color pattern_at(const Tuple& point) override;
+    Color pattern_at(const Tuple& point) const override;
 
     Color a_;
     Color b_;
@@ -62,7 +62,7 @@ class CheckersPattern : public Pattern
 public:
     CheckersPattern(const Color& a, const Color& b);
 
-    Color pattern_at(const Tuple& point) override;
+    Color pattern_at(const Tuple& point) const override;
 
     Color a_;
     Color b_;
@@ -73,8 +73,29 @@ class RadialGradientPattern : public Pattern
 public:
     RadialGradientPattern(const Color& a, const Color& b);
 
-    Color pattern_at(const Tuple& point) override;
+    Color pattern_at(const Tuple& point) const override;
 
     Color a_;
     Color b_;
+};
+
+class SolidPattern : public Pattern
+{
+public:
+    SolidPattern(const Color& color);
+
+    Color pattern_at(const Tuple&) const override;
+
+    Color color_;
+};
+
+class BlendedPattern : public Pattern
+{
+public:
+    BlendedPattern(Pattern& a, Pattern& b);
+
+    Color pattern_at(const Tuple& point) const override;
+
+    Pattern* a_;
+    Pattern* b_;
 };
